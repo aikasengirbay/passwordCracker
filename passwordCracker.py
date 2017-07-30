265fb276f533c23e44f1b0919de624ba5ba37f32
Aika Sengirbayeva <ai4h2u2@l-sfo9ebg944-m.gapinc.gap.com>
2017-07-30 13:10:50 -0700

    append part is working

--- passwordCracker.py
+++ passwordCracker.py
@@ -12,54 +12,39 @@ def find_password():
     
     your_password = raw_input("Enter a password > ")
     # your_password = my_list.index(your_password)
-    test_file = open('test.csv').read()
+    test_file = open('rockyou.txt').read()
+    # test_file = test_file.write()
     run_test = test_file.split("\r")
 
     if your_password in run_test:
-
-        print " {} Found ".format(your_password)
-    else:
-        print "Is not in the list"
-
-    # if your_password in my_list:
-    #     print 
-    #     print "*** {} *** password found".format(your_password)
-    #     print 
-    # elif your_password not in my_list:
-    #     print 
-    #     print "password not found"
-    #     print 
-    # my_list.append(your_password) 
+        print 
+        print "*** {} *** password found".format(your_password)
+        print 
+    elif your_password not in run_test:
+        print 
+        print "Password not found"
+        print
+    test_file = open('rockyou.txt', mode = "a")
+    test_file.write("\r" + your_password)
+    test_file.close()
   
 
 
 
-# def display_main_menu(my_list):
-#     """Displays main options, takes in user input, and calls view or add function."""
+def display_main_menu():
+    """Displays main options, takes in user input, and calls view or add function."""
 
-#     user_options = "Would you like to test your password, y/n ?"
-
-
-#     while True:
-#         answer = raw_input(user_options)
-#         # Collect input and include your if/elif/else statements here.
-#         if answer == "y":
-#             find_password(my_list) 
-#         elif answer == "n":
-#             break
-#         else:
-#             print "That is not an option"
-
-#-------------------------------------------------
- 
-# username = raw_input( "> ")
-
-# if username in open('test.csv').read():
-#     print " {} Found ".format(username)
-# else:
-#     print "Is not in the list"
-
-# display_main_menu()
+    user_options = "Would you like to test your password, y/n ? "
 
 
-find_password()
+    while True:
+        answer = raw_input(user_options)
+        if answer == "y":
+            find_password() 
+        elif answer == "n":
+            break
+        else:
+            print "That is not an option"
+
+
+display_main_menu()
